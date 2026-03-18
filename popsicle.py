aff75a0a7409c792517c0abb65cef9a63eee5dc7
qlenlen <1783619611@qq.com>
2026-03-18 21:40:27 +0800

    prop: less log

--- popsicle.py
+++ popsicle.py
@@ -104,6 +104,8 @@ ModuleDealer("DeBloat").perform_task()
 
 ModuleDealer("ChargeProp").perform_task()
 
+ModuleDealer("LessLogProp").perform_task()
+
 # repack and move to super
 img_vendor.pack_erofs().out2super()
 img_vendor.unlink().rm_content()
